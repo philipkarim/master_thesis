26205ef10e7de46d691131fb3d67e6a627761563
philipkarim <54434877+philipkarim@users.noreply.github.com>
2022-01-21 10:45:17 +0100

    ran it

--- main.py
+++ main.py
@@ -304,6 +304,10 @@ def train(H, ansatz, n_epochs, p_data, n_steps=10, lr=0.001, plot=True):
         #print(f'Final H, lets go!!!!: {H}')
 
         #Compute the dp_QBM/dtheta_i
+    
+    del optim
+    del varqite_train
+
     if plot==True:
         plt.plot(epoch_list, loss_list)
         plt.xlabel('Epoch')
@@ -353,7 +357,7 @@ def multiple_simulations(n_sims, ansatz2, epochs, target_data, l_r, steps):
     saved_error=np.zeros((n_sims, epochs))
     
     qbm_list=[]
-    np.random.seed(1234)
+    np.random.seed(321)
 
     for i in range(n_sims):
         print(f'Seed: {i} of {n_sims}')
@@ -443,7 +447,7 @@ def multiple_simulations(n_sims, ansatz2, epochs, target_data, l_r, steps):
     plt.savefig(str(l_r*1000)+'_all.png')
 
     return
-multiple_simulations(5, ansatz2, 20, p_data2, l_r=0.1, steps=10)
+multiple_simulations(4, ansatz2, 5, p_data2, l_r=0.1, steps=10)
 #exit()
 #multiple_simulations(10, ansatz2, 50, p_data2, l_r=0.1, steps=10)
 #multiple_simulations(3, ansatz2, 20, p_data2, l_r=0.01, steps=10)
