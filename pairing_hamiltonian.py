3c7748fbab73e33f677673cc4a3f05e425255c41
philipkarim <54434877+philipkarim@users.noreply.github.com>
2022-02-26 15:36:06 +0100

    Update pairing_hamiltonian.py

--- pairing_hamiltonian.py
+++ pairing_hamiltonian.py
@@ -114,8 +114,25 @@ def ci_matrix_pairing(n_pairs,n_basis,delta,g):
         H_mat[0,0] (float) - Reference energy
     """
     n_SD = int(special.binom(n_basis,n_pairs))
-    H_mat = np.zeros((n_SD,n_SD))
+    H0_mat = np.zeros((n_SD,n_SD))
+    H1_mat = np.zeros_like(H0_mat)
+    H_mat = np.zeros_like(H0_mat)
+
     S = stateMatrix(n_pairs,n_basis)
+    print(S)
+
+    #Without interaction along diagonal
+    for i in range(n_SD):            
+            H0_mat[i][i]+=2*delta*i
+
+    #Interaction part
+    for j in range(n_SD):
+        for k in range(n_SD):
+            if j==k:
+                H1_mat[j][k]+=-0.5*g*n_pairs
+            else:
+                H1_mat[j][k]+=-0.5*g
+
     for row in range(n_SD):
         bra = S[row,:]
         for col in range(n_SD):
@@ -124,7 +141,27 @@ def ci_matrix_pairing(n_pairs,n_basis,delta,g):
                 H_mat[row,col] += 2*delta*np.sum(bra - 1) - 0.5*g*n_pairs
             if n_pairs - np.intersect1d(bra,ket).shape[0] == 1:
                 H_mat[row,col] += -0.5*g
-    return(H_mat,H_mat[0,0])
+    return(H_mat, H0_mat, H1_mat)
+
+def orbital_possibilities(n_pairs, n_basis):
+    """
+    Find possible ways of filling the orbitals
+    """
+    unique_perms=1
+    #permutations=it.permutations(n_basis,n_pairs)
+    
+    #print(list(permutations))
+    L = []
+    states=range(1,n_basis+1)
+    for i in it.permutations(states,n_pairs):
+        L.append(i)
+    print(L)
+    return unique_perms
+
+orbital_possibilities(3, 4)
+
+
+    
 
 def stateMatrix(n_pairs,n_basis):
     L = []
@@ -142,7 +179,118 @@ def unique_rows(a):
     return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
 
 #Denne funker
-ci_matrix=ci_matrix_pairing(1, 4, 1, 0)
-#g=0 uten interaksjon
+#print('Uten interaction')
+ci_matrix_1, trash, trash=ci_matrix_pairing(3, 4, 1, 0)
+#print(ci_matrix_1)
 
+print('Kun interaction')
+ci_matrix_2, trash, trash=ci_matrix_pairing(3, 4, 0, 1)
+print(ci_matrix_2)
+print(trash)
+
+#print('Full interaction')
+ci_matrix, own_0, own_1=ci_matrix_pairing(3, 4, 1, 1)
+
+#g=0 uten interaksjon
+print(own_0)
+print(own_1)
+print(own_0+own_1)
 print(ci_matrix)
+print(np.all(ci_matrix==(ci_matrix_1+ci_matrix_2)))
+print(np.all(ci_matrix==(own_0+own_1)))
+
+
+
+def FCI(n,l,h,v,ret_all=False):
+    """
+    n - Number of particles
+    l - Number of spin orbitals
+    h - One-body matrix elements
+    v - Two-body matrix elements
+    """
+    n_SD = int(special.binom(l,n))
+    H = np.zeros((n_SD,n_SD),dtype=complex)
+    # Get occupied indices for all states
+    S = configurations(n,l) 
+    print(S)
+    for row,bra in enumerate(S):
+        for col,ket in enumerate(S):
+            if np.sum(np.equal(bra,ket)) == bra.shape:
+                # One-body contributions (Assumed diagonal)
+                for p in bra:
+                    H[row,col] += h[p,p]
+                # Two-body contributions
+                for p,q in it.combinations(bra,2):
+                    H[row,col] += v[p,q,p,q]
+            else:
+                # Two-body contributions
+                to_create = list(set(bra)-set(ket))
+                to_annihilate = list(set(ket)-set(bra))
+                eq = [i for i in bra if i not in to_create]
+                # One orbital different
+                if len(to_create) == 1 and len(to_annihilate) == 1:
+                    for p in eq:
+                        for c,a in zip(to_create,to_annihilate):
+                            H[row,col] += v[p,c,p,a]
+                # Two orbitals different
+                elif len(to_create) == 2 and len(to_annihilate) == 2:
+                    p,q = to_create
+                    r,s = to_annihilate
+                    H[row,col] += v[p,q,r,s]
+    Es,Vs = np.linalg.eigh(H)
+    if ret_all:
+        return Es,Vs
+    else:
+        return Es[0]
+
+def configurations(n,l):
+    states = []
+    for state in it.combinations(range(l),n):
+        states.append([orb for orb in state])
+    return np.asarray(states)
+
+
+
+
+
+def ci_matrix_pairing_sol(n_pairs,n_basis,delta,g):
+    """
+    Produces FCI matrix for Pairing Model.
+
+    Inputs:
+        n_pairs (int) - Number of electron pairs
+        n_basis (int) - Number of spacial basis states (spin-orbitals / 2)
+        delta (float) - one-body strength
+        g (float) - interaction strength
+    Outputs:
+        H_mat (array) - FCI matrix for pairing hamiltonian
+        H_mat[0,0] (float) - Reference energy
+    """
+    n_SD = int(special.binom(n_basis,n_pairs))
+    H_mat = np.zeros((n_SD,n_SD))
+    S = stateMatrix_sol(n_pairs,n_basis)
+    print(S)
+    for row in range(n_SD):
+        bra = S[row,:]
+        for col in range(n_SD):
+            ket = S[col,:]
+            if np.sum(np.equal(bra,ket)) == bra.shape:
+                H_mat[row,col] += 2*delta*np.sum(bra - 1) - 0.5*g*n_pairs
+            if n_pairs - np.intersect1d(bra,ket).shape[0] == 1:
+                H_mat[row,col] += -0.5*g
+    return(H_mat,H_mat[0,0])
+
+def stateMatrix_sol(n_pairs,n_basis):
+    L = []
+    states = range(1,n_basis+1)
+    for perm in it.permutations(states,n_pairs):
+        L.append(perm)
+    L = np.array(L)
+    L.sort(axis=1)
+    L = unique_rows_sol(L)
+    return(L)
+
+def unique_rows_sol(a):
+    a = np.ascontiguousarray(a)
+    unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
+    return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
