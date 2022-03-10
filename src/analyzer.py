37ae8700e6f7571203a705da1f1958ffdaef2156
Philip Niane <philipkarim1998@hotmail.com>
2022-03-10 19:26:47 +0100

    Working on it

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,15 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 sns.set_style("darkgrid")
 #x=np.load('results/arrays/learningrate0.507.npy', allow_pickle=True)
 
 def plot_fraud():
-    acc_train=np.load('results/fraud/acc_train_5050.npy', allow_pickle=True)
-    acc_test=np.load('results/fraud/acc_test_5050.npy', allow_pickle=True)
-    loss_train=np.load('results/fraud/loss_train_5050.npy', allow_pickle=True)
-    loss_test=np.load('results/fraud/loss_test_5050.npy', allow_pickle=True)
-
+    acc_train=np.load('results/fraud/acc_train_5050000109.npy', allow_pickle=True)
+    acc_test=np.load('results/fraud/acc_test_5050000109.npy', allow_pickle=True)
+    loss_train=np.load('results/fraud/loss_train_5050000109.npy', allow_pickle=True)
+    loss_test=np.load('results/fraud/loss_test_5050000109.npy', allow_pickle=True)
 
     plt.plot(list(range(len(acc_train))), acc_train, label='Train set')
     plt.plot(list(range(len(acc_test))), acc_test, label='Test set')
@@ -29,5 +29,41 @@ def plot_fraud():
     #plt.savefig('30 epochs.png')
     plt.show()
 
+def plot_multiple_samples():
+    """
+    Plots multiple data investigating the optimal parameters
+    """
+    acc_train_0001=np.load('results/fraud/acc_train_5050000109.npy', allow_pickle=True)
+    loss_train_0001=np.load('results/fraud/loss_train_5050000109.npy', allow_pickle=True)
 
-plot_fraud()
+    acc_train_001=np.load('results/fraud/acc_train_505000109.npy', allow_pickle=True)
+    loss_train_001=np.load('results/fraud/loss_train_505000109.npy', allow_pickle=True)
+
+    acc_train01=np.load('results/fraud/acc_train_50500109.npy', allow_pickle=True)
+    loss_train01=np.load('results/fraud/loss_train_50500109.npy', allow_pickle=True)
+
+    acc_train_000107=np.load('results/fraud/acc_train_5050000107.npy', allow_pickle=True)
+    loss_train_000107=np.load('results/fraud/loss_train_5050000107.npy', allow_pickle=True)
+
+    plt.plot(list(range(len(acc_train_0001))), acc_train_0001, label=r'$\gamma=0.001$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(acc_train_001))), acc_train_001, label=r'$\gamma=0.01$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(acc_train01))), acc_train01, label=r'$\gamma=0.1$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(acc_train_000107))), acc_train_000107, label=r'$\gamma=0.001$, $m_1=0.7$, $m_2=0.99$')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    #plt.savefig('30 epochs.png')
+    plt.show()
+
+    plt.plot(list(range(len(loss_train_0001))), loss_train_0001, label=r'$\gamma=0.001$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(loss_train_001))), loss_train_001,  label=r'$\gamma=0.01$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(loss_train01))), loss_train01, label=r'$\gamma=0.1$, $m_1=0.9$, $m_2=0.999$')
+    plt.plot(list(range(len(loss_train_000107))), loss_train_000107,  label=r'$\gamma=0.001$, $m_1=0.7$, $m_2=0.99$')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    #plt.savefig('30 epochs.png')
+    plt.show()
+
+#plot_fraud()
+plot_multiple_samples()
